5f5f0833126d56fd2b656fd475d1a4612a8e2208
Julius170 <juliusyoola5@gmail.com>
2022-11-13 03:48:06 +0100

    completed

---
+++ movies/migrations/0002_movie_date_created.py
@@ -0,0 +1,19 @@
+
+import datetime
+from django.db import migrations, models
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('movies', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='movie',
+            name='date_created',
+            field=models.DateTimeField(default=datetime.datetime(2022, 11, 5, 12, 41, 22, 104783, tzinfo=utc)),
+        ),
+    ]
---
+++ movies/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
+
+# Create your views here.
+def index(request):
+    movies = Movie.objects.all()
+    return render(request, "movies/index.html", {"movies": movies})
+    # output = ", ".join([m.title for m in movies])
+
+
+def detail(request, movie_id):
+
+    movie = get_object_or_404(Movie, id=movie_id)
+    return render(request, "movies/detail.html", {"movies": movie})
